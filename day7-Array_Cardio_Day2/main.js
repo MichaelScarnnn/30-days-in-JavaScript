6d2f69b19a24a349508dc985d0d9d2f14e721717
akifkrdg <107864022+MichaelScarnnn@users.noreply.github.com>
2023-05-23 21:10:32 +0300

    files are added

---
+++ day7-Array_Cardio_Day2/main.js
@@ -0,0 +1,48 @@
+    // ## Array Cardio Day 2
+
+    const people = [
+      { name: 'Wes', year: 1988 },
+      { name: 'Kait', year: 1986 },
+      { name: 'Irv', year: 1970 },
+      { name: 'Lux', year: 2015 },
+      { name: 'Michael', year: 1982 },
+      { name: 'Akif', year: 1997},
+      { name: 'Scarn', year: 2022}
+    ];
+
+    const comments = [
+      { text: 'Love this!', id: 523423 },
+      { text: 'Super good', id: 823423 },
+      { text: 'You are the best', id: 2039842 },
+      { text: 'Ramen is my fav food ever', id: 123523 },
+      { text: 'Nice Nice Nice!', id: 542328 },
+      { text: 'That was she said', id: 23 },
+    ];
+
+    // Some and Every Checks
+    // Array.prototype.some() // is at least one person 19 or older?
+    const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+    console.log({isAdult});
+    
+    /* const areAdult = people.filter(person => ((new Date()).getFullYear()) - person.year >= 19);
+    console.log({areAdult}); */
+    
+    // Array.prototype.every() // is everyone 19 or older?
+    const allAdult = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+    console.log({allAdult});
+
+    // Array.prototype.find()
+    // Find is like filter, but instead returns just the one you are looking for
+    // find the comment with the ID of 23
+    const comment = comments.find(comment => comment.id === 23);
+    console.log(comment);
+
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    const commentIndex = comments.findIndex(comment => comment.id === 823423);
+    console.log(commentIndex);
+    // delete the comment with the ID of 823423
+    comments.splice(commentIndex, 1);
+    console.table(comments);
+
+  
